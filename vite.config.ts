ce3e53821f9dad101fcd0e18098a17ee39d51746
MICH <michstudio.com@gmail.com>
2026-01-20 16:38:17 -0600

    feat: Configure Vite for production and development
    Updates Vite configuration to optimize for production builds, including disabling sourcemaps and enabling emptyOutDir. Also, configures the development server to listen on all host interfaces. Adjusts TypeScript compiler options for broader compatibility and stricter type checking. Updates import maps for dependencies to use caret versioning.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
   build: {
     outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true,
   },
   root: '.',
 });
